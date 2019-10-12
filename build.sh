8d99a89149f54118ae2e78147d0db882fd18e247
Kevin M. Gallagher <kevingallagher@gmail.com>
2019-10-12 02:06:18 -0700

    Generate and use a random TOR_CONTROL_PASSWD

--- build.sh
+++ build.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
-docker build --no-cache --compress --add-host torproxy:127.0.0.1 --label 'tor' --network host -t torproxy .
-cd files && docker-compose up -d --force-recreate torproxy
+export DOCKER_HOST="unix:///var/run/docker.sock"
+export TOR_CONTROL_PASSWD="$(openssl rand -hex 16)"
+docker-compose --file files/docker-compose.yml --project-directory files --project-name torproxy --verbose --log-level INFO --host "${DOCKER_HOST}"  build --no-cache --compress --build-arg TOR_CONTROL_PASSWD=${TOR_CONTROL_PASSWD}
+docker-compose --file files/docker-compose.yml --project-directory files --project-name torproxy --verbose --log-level INFO --host "${DOCKER_HOST}"  up --detach torproxy
 
 
 
